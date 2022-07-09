184b5b01403a9a997f3ccdbe15b61d6bd7657e0c
Egito Filho <egitofilho4@gmail.com>
2022-07-09 20:26:51 -0300

    modificactions colors/img

--- app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
+++ app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
@@ -1,6 +1,7 @@
 package br.com.alura.orgs.dao
 
 import br.com.alura.orgs.model.Produto
+import java.math.BigDecimal
 
 class ProdutosDao {
 
@@ -13,7 +14,11 @@ class ProdutosDao {
     }
 
     companion object {
-        private val produtos = mutableListOf<Produto>()
+        private val produtos = mutableListOf<Produto>(
+            Produto(nome = "Salada de frutas",
+            descricao = "laranja, maçã e uvas",
+            valor = BigDecimal("19.83")
+            )
+        )
     }
-
 }
